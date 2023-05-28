e4b72fd29d442fb6cce92d93bc031e6a5dcbafa0
Bikash Pradhan <121172268+Bikash-Pradhan-git@users.noreply.github.com>
2023-05-28 09:27:57 +0000

    With the function "startsWith" help you to find the word start with this or not

---
+++ Start.java
@@ -0,0 +1,8 @@
+public class Start{
+  public static void main(String args[]){
+    String str="This is demo";
+
+    System.out.println("Is this string start with \"This\"?"+str.startsWith("This"));
+     System.out.println("Is this string start with \"demo\"?"+str.startsWith("demo"));
+  }
+}
