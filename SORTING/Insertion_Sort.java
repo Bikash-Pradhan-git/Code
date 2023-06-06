e9f1b8d2f9fe5a2c166f339b42035df9ad86c8a9
Bikash Pradhan <121172268+Bikash-Pradhan-git@users.noreply.github.com>
2023-06-06 15:12:29 +0000

    ALL ARE SORTING TECHNIQUE HAVING DIFFERENT APPROACH

---
+++ SORTING/Insertion_Sort.java
@@ -0,0 +1,36 @@
+package SORTING;
+
+import java.util.*;
+public class Insertion_Sort {
+  public static void sorting(int arr[]){
+    for(int i=1;i<arr.length;i++){
+        int curr=arr[i];
+        int prev=i-1;
+        while(prev>=0 && arr[prev]>curr){
+          arr[prev+1]=arr[prev];
+          prev--;
+        }
+        arr[prev+1]=curr;
+    }
+  }
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the size of an array");
+    int n=sc.nextInt();
+
+    int arr[]=new int[n];
+
+    System.out.println("Enter the data in the array");
+    for(int i=0;i<arr.length;i++){
+      arr[i]=sc.nextInt();
+    }
+    //Call to function for sorting
+    sorting(arr);
+
+    //Display the array
+    for(int i=0;i<arr.length;i++){
+      System.out.print(arr[i]+" ");
+    }
+    sc.close();
+  }
+}
